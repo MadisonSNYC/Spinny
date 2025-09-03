b2867be3c4db3085c1bdead6745c265a1c3bdf90
MadisonSNYC <madison@thenycagentteam.com>
2025-09-03 05:55:54 -0400

    Simplify App.jsx - remove all unused props and state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { EnhancedHelixProjectsShowcase } from './components/EnhancedHelixProjectsShowcase.jsx';
 import { HelixProvider } from './contexts/HelixContext.jsx';
 import { useEffectsCompat } from './hooks/useMigrationBridge.js';
 import './App.css';
 
 function AppContent() {
-  const { effects, toggleEffect, resetEffects, undoEffects, redoEffects, canUndo: canUndoEffects, canRedo: canRedoEffects, setPlacementStrength, setRepeatTurns } = useEffectsCompat();
+  const { effects } = useEffectsCompat();
 
   return (
     <div className="App relative">
-      {/* DevPanel removed for streamlined UI */}
-      
       <EnhancedHelixProjectsShowcase 
         autoRotate={true}
-        scrollDriven={false}
         effects={effects}
-        onEffectToggle={toggleEffect}
-        onReset={resetEffects}
-        onUndo={undoEffects}
-        onRedo={redoEffects}
-        canUndo={canUndoEffects}
-        canRedo={canRedoEffects}
-        setPlacementStrength={setPlacementStrength}
-        setRepeatTurns={setRepeatTurns}
       />
-      
-      {/* All debug components removed for production */}
     </div>
   );
 }
